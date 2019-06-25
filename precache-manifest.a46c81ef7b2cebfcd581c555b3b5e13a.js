efc3c9dd7750d5ed798e04b5974f4964f350b120
robert.reppel <robert.reppel@twotonic.net>
2019-06-25 17:24:27 +0200

    Updates

---
+++ precache-manifest.a46c81ef7b2cebfcd581c555b3b5e13a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe007a0e5290e2590dddc8df66b4ed9a",
+    "url": "/kubel/index.html"
+  },
+  {
+    "revision": "6c1c5a19964235f355d9",
+    "url": "/kubel/static/css/main.5598cd8a.chunk.css"
+  },
+  {
+    "revision": "3fbe38325cb2606ddbb5",
+    "url": "/kubel/static/js/2.c19a4a28.chunk.js"
+  },
+  {
+    "revision": "6c1c5a19964235f355d9",
+    "url": "/kubel/static/js/main.6f144342.chunk.js"
+  },
+  {
+    "revision": "a4689bcb5e0836369462",
+    "url": "/kubel/static/js/runtime~main.707b6072.js"
+  }
+]);
