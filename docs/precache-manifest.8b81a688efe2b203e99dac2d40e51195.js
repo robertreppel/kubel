ec7b55d4fea5b367075c3412908b687873fe8f24
robert.reppel <robert.reppel@twotonic.net>
2019-06-25 17:06:53 +0200

    Use Github Pages ./docs folder for builds so that Github pages work

---
+++ docs/precache-manifest.8b81a688efe2b203e99dac2d40e51195.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7351898bf6d0ed415ee079546e73cace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5b538342bef23bf27d0",
+    "url": "/static/css/main.5598cd8a.chunk.css"
+  },
+  {
+    "revision": "3fbe38325cb2606ddbb5",
+    "url": "/static/js/2.c19a4a28.chunk.js"
+  },
+  {
+    "revision": "a5b538342bef23bf27d0",
+    "url": "/static/js/main.1db9a14d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
