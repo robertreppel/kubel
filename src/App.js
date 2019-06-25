b3b10350427f9859cf2db0a179f57e0cf491b083
robert.reppel <robert.reppel@twotonic.net>
2019-06-25 13:58:09 +0200

    Download

--- src/App.js
+++ src/App.js
@@ -18,20 +18,53 @@ const App = () => {
     localStorage.setItem('words', wordsJson);
   }, [words]);
 
+  const createNew = () => {
+    const retVal = window.confirm("Creating a new Kubel will delete everything here.");
+    if (retVal === true) {
+      localStorage.clear();
+      window.location.reload();
+    }
+  };
+
+  const exportJson = () => {
+    let projectJson = []
+    for (var key in localStorage) {
+      const item = JSON.parse(localStorage.getItem(key))
+      projectJson.push({ key: key, item: item })
+    }
+    let fileName = window.prompt("Export As:")
+    if (!fileName) {
+      fileName = "Untitled"
+    }
+    if (!fileName.includes(".")) {
+      fileName = `${fileName}.json`
+    }
+    download(JSON.stringify(projectJson), fileName, 'text/json');
+
+  };
+
+  function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], { type: contentType });
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+  }
+
+
   return (
     <div className="page">
       <div className="App-header">
         <div className="menuItem"><h2>Kubel</h2></div>
         <div className="menuItem">
-          <button onClick={() => {
-            const retVal = window.confirm("Creating a new Kubel will delete everything here.");
-            if (retVal == true) {
-              localStorage.clear()
-              window.location.reload()
-            } 
-          }}>
+          <button onClick={createNew}>
             New
             </button>
+        </div>
+        <div className="menuItem">
+          <button onClick={exportJson}>
+            Export
+          </button>
         </div>
       </div>
       <hr />
