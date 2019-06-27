268f80cc428e2c6b8c1dbc2f1cdf610bd1b0b512
robert.reppel <robert.reppel@twotonic.net>
2019-06-27 19:00:23 +0200

    Onboarding better.

--- src/features/BoundedContexts/BoundedContextList.js
+++ src/features/BoundedContexts/BoundedContextList.js
@@ -24,24 +24,26 @@ export const BoundedContextList = (props) => {
       <div>
         <span><h4 style={{ display: "inline-block", marginRight: "10px", color: " #F8FAFB" }}>Contexts</h4></span><NewContext createNewBoundedContext={createNewBoundedContext} />
       </div>
-      { contexts.length === 0 ? <OnboardingInstructions /> :
-      <React.Fragment>
-      <hr style={{ color: "#F8FAFB" }}/>
-      <div className="contextList">
-        {contexts.map(context => {
-          return (<BoundedContext key={context.id} setCurrentContext={props.setCurrentContext} currentContext={props.currentContext} context={context} />);
-        })}
-      </div>
-      </React.Fragment>
+      {contexts.length === 0 ? <OnboardingInstructions /> :
+        <React.Fragment>
+          <hr style={{ color: "#F8FAFB" }} />
+          <div className="contextList">
+            {contexts.map(context => {
+              return (<BoundedContext key={context.id} setCurrentContext={props.setCurrentContext} currentContext={props.currentContext} context={context} />);
+            })}
+          </div>
+        </React.Fragment>
       }
     </div>);
 };
 
 
 const OnboardingInstructions = () => {
-  return(<div className="onboardingText">
-    <div className="onboardingTextLine">1. Drag &amp; Drop terms which belong together into groups. </div>
-    <div className="onboardingTextLine">2. Create &amp; select bounded contexts.</div> 
-    <div className="onboardingTextLine">3. Click terms to mark them as belonging to the currently selected context.</div>
-    </div>)
+  return (
+    <ol className="onboardingText">
+      <li className="onboardingTextLine">Drag &amp; Drop terms which belong together into groups. </li>
+      <li className="onboardingTextLine">Create &amp; select bounded contexts.</li>
+      <li className="onboardingTextLine">Click terms to mark them as belonging to the currently selected context.</li>
+    </ol>
+  )
 }
